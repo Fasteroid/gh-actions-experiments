87a6cf382528409515e90fa56f0419f68df5a276
Fasteroid <29342750+Fasteroid@users.noreply.github.com>
2025-07-27 19:05:22 -0400

    cursed

---
+++ resources/time/git.sh
@@ -0,0 +1,2 @@
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"W
---
+++ resources/time/push.sh
@@ -0,0 +1,17 @@
+
+mkdir -p __time
+cd __time
+
+# Initialize a new git repo in the subdirectory
+git init
+git remote add origin ${{ github.server_url }}/${{ github.repository }}.git
+
+# Get the time
+date > time
+
+# Add and commit the single file
+git add "time"
+git commit -m "update the time"
+
+# Force push to target branch
+git push origin HEAD:data.time --force
