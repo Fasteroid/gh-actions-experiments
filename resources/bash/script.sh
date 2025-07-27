8f2aedc57c265fde34980a9da7768fe8a39ab825
Fasteroid <29342750+Fasteroid@users.noreply.github.com>
2025-07-27 17:38:53 -0400

    yeet

---
+++ resources/bash/script.sh
@@ -0,0 +1,1 @@
+echo "bash.sh :3"
---
+++ triggers/trigger-hello.sh
@@ -0,0 +1,12 @@
+token=$(cat ./token)
+
+curl -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token $token" \
+  https://api.github.com/repos/fasteroid/gh-actions-experiments/dispatches \
+  -d '{
+    "event_type": "hello",
+    "client_payload": {
+      "message": "Triggered from cURL!"
+    }
+  }'
